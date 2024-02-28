7b73b95d422b0845de5bba771e33775d167d9b39
kebabnekaise <emsi06001@utb.vaxjo.se>
2024-02-28 12:52:47 +0100

    Add project files.

---
+++ inluppg6.6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace inluppg6_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in en text");
+            string inputString = Console.ReadLine();
+            OmvandlaVartannat(inputString);
+        }
+
+        static void OmvandlaVartannat(string inputString)
+        {
+            string[] splitString = inputString.Split(' ');
+            for (int i = 0; i <splitString.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.Write(splitString[i].ToUpper() + ' ');
+                }
+                else
+                {
+                    Console.Write(splitString[i] + ' ');
+                }
+            }
+        }
+    }
+}
